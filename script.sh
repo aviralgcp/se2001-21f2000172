687209dfab68ab3a016a5abc14dde5feb74eb9bf
aviralgcp <aviralkgcp@gmail.com>
2023-12-21 15:02:29 +0000

    added script.sh

--- script.sh
+++ script.sh
@@ -1,13 +1,5 @@
 #!/bin/bash
-
-
-
 # Export the GitHub username
-
 export GITHUB_USERNAME="aviralgcp"
-
-
-
 # Export the GitHub repository name
-
-export GITHUB_REPOSITORY="se2001-<roll_21f2000172"
+export GITHUB_REPOSITORY="se2001-roll_21f2000172"
